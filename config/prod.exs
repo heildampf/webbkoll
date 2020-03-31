5c85d4eb3e18c0d142fece989e2207df7e3cadc5
heildampf <heildampf@gmail.com>
2020-03-31 15:58:35 +0200

    Update prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -80,5 +80,5 @@ config :webbkoll,
   # See https://github.com/grempe/ex_rated
   rate_limit_client: %{"scale" => 60_000, "limit" => 20},
   rate_limit_host: %{"scale" => 60_000, "limit" => 5},
-  geoip_db_url: "https://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.mmdb.gz",
-  geoip_db_md5_url: "https://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.md5"
+  geoip_db_url: "https://web.archive.org/web/20191227182412/https://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.tar.gz",
+  #geoip_db_md5_url: "https://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.md5"
